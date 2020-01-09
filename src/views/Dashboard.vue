cfef20c4745ddd699c63e9f3bc8a7081ad919c26
Teri Fash <teri.fash@gmail.com>
2020-01-09 14:24:52 +0200

    Clear project

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="dashboard">
+    <h1>This is an dashboard page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'dashboard'
+}
+</script>
