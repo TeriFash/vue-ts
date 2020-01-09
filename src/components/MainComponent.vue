b73a9341a1f62ea01ae1711865070ae325e76ca6
Teri Fash <teri.fash@gmail.com>
2020-01-09 17:56:47 +0200

    test

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ getData() }}</h1> -->
+
+    <p v-text="getData()"></p>
   </div>
 </template>
 
@@ -10,6 +12,16 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class MainComponent extends Vue {
   @Prop() private msg!: string;
+
+  urlData = 'https://jsonplaceholder.typicode.com/posts'
+  myData: { name: string } = { name: '' }
+
+  getData () {
+    fetch(this.urlData)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(data => data)
+  }
 }
 </script>
 
